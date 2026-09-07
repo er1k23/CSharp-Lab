f0548cbcfea54455a1e8ce6d3a213f986f57b76b
Erik <erikrumian@gmail.com>
2026-09-07 15:38:36 +0400

    Add SocialNetwork project

---
+++ SocialNetwork/SocialNetwork/Program.cs
@@ -0,0 +1,17 @@
+﻿using Npgsql;
+
+namespace SocialNetwork;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = "Host=localhost;Port=5433;Database=social-network;Username=admin;Password=admin123";
+
+        using var connection = new NpgsqlConnection(connectionString);
+        
+        connection.Open();
+        
+        Console.WriteLine("Successfully connected to PostgreSQL!");
+    }
+}
