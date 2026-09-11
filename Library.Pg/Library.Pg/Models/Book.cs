103c2b4885d7306830eb31f88c41773ef5468d62
Erik <erikrumian@gmail.com>
2026-09-11 13:08:33 +0400

    feat: add Book entity

--- Library.Pg/Library.Pg/Models/Book.cs
+++ Library.Pg/Library.Pg/Models/Book.cs
@@ -2,5 +2,15 @@ namespace Library.Pg.Models;
 
 public class Book
 {
+    public int BookId { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Author { get; set; } = string.Empty;
     
+    public decimal Price { get; set; }
+    
+    public DateTime PublishedOn { get; set; }
+    
+    public bool IsRead { get; set; }
 }
