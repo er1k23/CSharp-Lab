ac0a7221caea16412078f87cbbb1fb2f5335714a
Erik <erikrumian@gmail.com>
2026-09-21 20:45:40 +0400

    feat: add job request validation

--- JobRecruitmentApi/Models/CreateJobRequest.cs
+++ JobRecruitmentApi/Models/CreateJobRequest.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace JobRecruitmentApi.Models;
 
 public class CreateJobRequest
 {
+    [Required]
+    [StringLength(100)]
     public string Title { get; set; } = string.Empty;
+    [Required]
+    [StringLength(1000)]
     public string Description { get; set; } = string.Empty;
+    [Range(0,double.MaxValue)]
     public decimal Salary { get; set; }
 }
